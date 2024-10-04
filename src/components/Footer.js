3b422d93e16fcfb9fa761919e6f77d70910ec96f
ZiaAlam7 <codewithalam@gmail.com>
2024-10-04 18:39:16 +0500

    Second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,12 @@ const Footer = () => {
   return (
 <div className='footer'>
             <div className="details">
-                Made By Zia Alam <a href="https://www.linkedin.com/in/zia-alam-102572227/" target="_blank" rel="noreferrer noopener">LinkedIN</a>
-            </div>
+                Made By Zia Alam <a className='linked' href="https://www.linkedin.com/in/zia-alam-102572227/" target="_blank" rel="noreferrer noopener">LinkedIN</a>
+                </div>
+                <div className='credit'>
+                <a href="https://www.flaticon.com/free-icons/online-game" title="online game icons" target="_blank" rel="noopener">Attribution : Word icon created by Freepik - Flaticon</a> 
+                </div>
+            
         
            
         </div>
